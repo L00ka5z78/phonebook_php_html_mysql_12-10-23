5bb6c126c916a9a15411968ba4fb291f0eac85b2
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-12 13:16:04 +0200

    html form added 12.10.23 13:15

--- create.php
+++ create.php
@@ -14,12 +14,50 @@
     <div class="container my-5">
         <h2>New Client</h2>
 
-        <form>
-
+        <form method="post">
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label" for="">First Name</label>
                 <div class="col-sm-6">
                     <input type="text" class="form-control" name="name" value="">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Last Name</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="surname" value="">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">E-mail</label>
+                <div class="col-sm-6">
+                    <input type="email" class="form-control" name="email" value="">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Phone Number</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="phone" value="">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Address</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="address" value="">
+                </div>
+            </div>
+
+
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a class="btn btn-outline-primary" href="/phphone/index.php" role="button">Cancel</a>
                 </div>
             </div>
         </form>
