441dd9de9ec744280817d0939cba548c473e2af6
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-12 14:41:58 +0200

    db connection established. taken from index file 12.10.23 14:41

--- create.php
+++ create.php
@@ -1,4 +1,17 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "phonebook";
+
+// connect database connection
+$connection = new mysqli($servername, $username, $password, $database);
+
+// check db connection
+if ($connection->connect_error) {
+    die("Connection failed" . $connection->connect_error);
+}
+
 $name = '';
 $surname = '';
 $email = '';
