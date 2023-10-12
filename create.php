92aa2d606659652d7f6b007c64102d16dc5b70dd
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-12 13:10:42 +0200

    create client form in progress 12.10.23 13:10

---
+++ create.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Client</title>
+    <!-- bootstrap styling taken from web -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container my-5">
+        <h2>New Client</h2>
+
+        <form>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">First Name</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="name" value="">
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
