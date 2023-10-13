d6f9872f1d70242f563533d645f86be656300070
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-13 16:00:27 +0200

    navbar footer and small banner added 13.10.23 16:00

---
+++ public/banner.php
@@ -0,0 +1,9 @@
+<style>
+    #banner {
+        width: 100%;
+        height: 20rem;
+        object-fit: cover;
+        margin-bottom: 1rem
+    }
+</style>
+<img class="img-fluid" id="banner" src="https://l00ka5z78-cs50-final-project-careers-website.geriatrichardma.repl.co/static/banner.jpg" alt="imgage">
---
+++ templates/footer.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+
+            <li class="nav-item"><a target="_blank" href="https://github.com/L00ka5z78" class="nav-link px-2 text-muted">Github</a></li>
+
+            <li class="nav-item"><a target="_blank" href="https://l00ka5z78.github.io/my-portfolio-page/" class="nav-link px-2 text-muted">Portfolio page</a></li>
+
+            <li class="nav-item"><a target="_blank" href="https://www.linkedin.com/in/%C5%82ukasz-bolach/" class="nav-link px-2 text-muted">LinkedIn</a></li>
+        </ul>
+        <p class="text-center text-muted">&copy; 2023, Lukasz Inc</p>
+    </footer>
+</div>
